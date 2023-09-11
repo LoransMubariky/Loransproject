a5a28230b4d4f23241b4b25525abf259436748af
LoransMubariky <pigypigy508@gmail.com>
2023-09-11 09:58:06 +0300

    login page

---
+++ app/src/main/java/com/example/loransmubarikyproj/LoginActivity.java
@@ -0,0 +1,57 @@
+package com.example.loransmubarikyproj;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+EditText emailEditText;
+EditText passwordEditText;
+ Button loginButton;
+TextView newAccountButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        emailEditText=findViewById(R.id.ETEmail);
+        passwordEditText=findViewById(R.id.ETPassword);
+        loginButton=findViewById(R.id.BTMLogin);
+        newAccountButton=findViewById(R.id.BTRegister);
+        newAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,SignupActivity.class));
+            }
+        });
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final FirebaseAuth mAuth=FirebaseAuth.getInstance();
+                mAuth.signInWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = mAuth.getCurrentUser();
+
+                                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
+
+                                } else {
+
+                                    Log.d("error","erro");
+
+                                }
+                            }
+                        });
+            }
+        });
+    }
+}
